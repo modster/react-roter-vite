5b6e650e0b5183e03d5102c40b7012b12ea2395c
EM Greeff <emgreeff@datadash.cloud>
2025-03-02 03:24:19 -0700

    On branch main Initial commit Changes to be committed: 	new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   deno.json 	new file:   deno.lock 	new file:   index.html 	new file:   public/vite-deno.svg 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.tsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   vite.config.ts

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+// import { StrictMode } from 'react'
+import { BrowserRouter } from "react-router";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
